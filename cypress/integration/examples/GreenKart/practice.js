9dd91cb183d46d91ed4b59ae6460daead4cd05d5
sanmicad@gmail.com <sanmicad@gmail.com>
2020-01-22 15:40:07 +0000

    mike

--- cypress/integration/examples/GreenKart/practice.js
+++ cypress/integration/examples/GreenKart/practice.js
@@ -7,7 +7,8 @@ describe('My First practice Suite', function() {
         cy.get('.gLFyf').type('proquest')
         cy.get('.FPdoLc > center > .gNO89b').click({force:true})
         cy.contains('ProQuest | Databases, EBooks and Technology for Research').click()
-        cy.get('.hidden-xs > .dropdown-toggle > .fa').click()
+        cy.wait(5000)
+        cy.get('.hidden-xs > .dropdown-toggle > .fa').click({force:true})
         cy.get('#search-form > .input-group > .twitter-typeahead > .form-control').type('QA')
         cy.get('#search-form > .input-group > .input-group-btn > .btn > .fa').click()
 
